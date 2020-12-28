4bbf73b3e389b71e19298b854d6476a1c5c1c17f
kientrumhodo <kientrumhodo997@gmail.com>
2020-12-28 20:15:01 +0700

    1

--- app.js
+++ app.js
@@ -34,4 +34,4 @@ const index_controllers = require('./controllers/index_controllers.js');
 app.use('/',index_controllers);
 
 
-app.listen(3000,()=>{console.log('on server')})
+app.listen(process.env.PORT,()=>{console.log('on server')})
